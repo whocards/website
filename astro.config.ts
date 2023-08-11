0030f1b89cb5dc098df2135d674fee9b4259ee41
acharlop <avicharlop@gmail.com>
2023-08-11 23:34:37 +0200

    feat: add robots.txt

--- astro.config.ts
+++ astro.config.ts
@@ -2,10 +2,12 @@
 import {defineConfig} from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import solid from '@astrojs/solid-js'
-// import image from '@astrojs/image'
+import image from '@astrojs/image'
 // import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import {SITE_URL} from './scripts/env'
+
+import robotsTxt from 'astro-robots-txt'
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,10 +16,11 @@ export default defineConfig({
   integrations: [
     solid(),
     tailwind(),
-    // image({
-    //   serviceEntryPoint: '@astrojs/image/sharp',
-    // }),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
     // mdx(),
     sitemap(),
+    robotsTxt(),
   ],
 })
