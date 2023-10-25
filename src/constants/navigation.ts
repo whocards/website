6f52fc1fa50f7ef73b8d0ab9d64cc34d6f6fd669
acharlop <avicharlop@gmail.com>
2023-10-25 18:39:58 +0200

    feat: add footer

---
+++ src/constants/navigation.ts
@@ -0,0 +1,20 @@
+export type Link = {
+  href: string
+  title: string
+  button?: boolean
+}
+
+export const mainLinks: Link[] = [
+  {href: '/', title: 'Home'},
+  {href: '/mission', title: 'Mission'},
+  {href: '/print', title: 'Print'},
+  {href: '/preorder', title: 'Pre-Order', button: true},
+]
+
+export const preorderLinks: Link[] = [
+  {href: '#what-is-who-cards', title: 'About'},
+  {href: '#how-to-play', title: 'How to Play'},
+  {href: '#support-us', title: 'Pricing'},
+  {href: '#faqs', title: 'FAQs'},
+  {href: '#support-us', title: 'Pre-Order Now', button: true},
+]
