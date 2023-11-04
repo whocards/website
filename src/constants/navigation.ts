4712187b49ae65061e516b75b079cadb0c1d04cf
acharlop <avicharlop@gmail.com>
2023-11-04 17:32:21 +0100

    feat: contact page

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -2,6 +2,7 @@ export type Link = {
   href: string
   title: string
   button?: boolean
+  icon?: string
 }
 
 export const mainLinks: Link[] = [
@@ -18,3 +19,18 @@ export const preorderLinks: Link[] = [
   {href: '/preorder#faqs', title: 'FAQs'},
   {href: '/preorder#support-us', title: 'Pre-Order Now', button: true},
 ]
+
+export const socialLinks: Link[] = [
+  {icon: 'mdi:linkedin', title: 'Linkedin', href: 'https://www.linkedin.com/company/whocards'},
+  // { icon: 'mdi:twitter', href: 'https://twitter.com/whocards' },
+  {icon: 'entypo-social:facebook', title: 'Facebook', href: 'https://facebook.com/whocards'},
+  {icon: 'mdi:instagram', title: 'Instagram', href: 'https://instagram.com/who_cards'},
+]
+
+export const contactLinks: Link[] = [
+  ...socialLinks,
+  {icon: 'mdi:email', title: 'Email', href: 'mailto:hello@whocards.cc'},
+  {icon: 'mdi:github', title: 'Github', href: 'https://github.com/whocards'},
+]
+
+export const legalLinks: Link[] = [{title: 'Privacy Policy', href: '/legal/pp'}]
