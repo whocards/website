3419df14c1608f9ef636566ec086c9cc8fa3c664
acharlop <avicharlop@gmail.com>
2023-11-30 00:25:16 +0100

    migrate pre-order page to gift page

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -9,15 +9,15 @@ export const mainLinks: Link[] = [
   {href: '/', title: 'Home'},
   {href: '/mission', title: 'Mission'},
   {href: '/print', title: 'Print'},
-  {href: '/preorder', title: 'Pre-Order Now', button: true},
+  {href: '/gift', title: 'Order Now', button: true},
 ]
 
 export const preorderLinks: Link[] = [
-  {href: '/preorder#what-is-who-cards', title: 'About'},
-  {href: '/preorder#how-to-play', title: 'How to Play'},
-  {href: '/preorder#support-us', title: 'Pricing'},
-  {href: '/preorder#faqs', title: 'FAQs'},
-  {href: '/preorder#support-us', title: 'Pre-Order Now', button: true},
+  {href: '/gift#what-is-whocards', title: 'About'},
+  {href: '/gift#how-to-play', title: 'How to Play'},
+  {href: '/gift#support-us', title: 'Pricing'},
+  {href: '/gift#faqs', title: 'FAQs'},
+  {href: '/gift#support-us', title: 'Order Now', button: true},
 ]
 
 export const socialLinks: Link[] = [
